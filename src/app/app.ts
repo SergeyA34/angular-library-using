2d9a40dd663ddd3c3b8f5c6b9df878b6b1dabe66
Sergey Arefkin <arefkins@globalbit.co.il>
2026-05-15 15:01:57 +0400

    example app with the slotted table

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,27 @@ import { IgdsButton } from "@igds/angular/button";
 import { IgdsRadioGroup } from "@igds/angular/radio-group";
 import { IgdsRadio } from "@igds/angular/radio";
 import { IgdsInput } from "@igds/angular/input";
+import { IgdsTable } from "@igds/angular/table";
+import { IgdsTableHeader } from "@igds/angular/table-header";
+import { IgdsTableRow } from "@igds/angular/table-row";
+import { IgdsTableCell } from "@igds/angular/table-cell";
+import { IgdsDropdown } from "@igds/angular/dropdown";
+import { Option } from "@igds/core-web/dropdown";
 
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule, IgdsButton, IgdsRadioGroup, IgdsRadio, IgdsInput],
+  imports: [
+    ReactiveFormsModule,
+    IgdsButton,
+    IgdsRadioGroup,
+    IgdsRadio,
+    IgdsInput,
+    IgdsTable,
+    IgdsTableHeader,
+    IgdsTableRow,
+    IgdsTableCell,
+    IgdsDropdown,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -17,6 +34,7 @@ export class App {
   inputValue2 = '';
   checkboxValue = false;
   variant = signal<typeof this.buttonVariants[number]>('primary');
+  selectedRows = signal<string[]>([]);
   buttonVariants = [
     'alternative',
     'link',
@@ -24,6 +42,21 @@ export class App {
     'primary',
     'secondary',
   ] as const;
+  dropdownOptions: Option[] = [
+    {
+      id: '1',
+      label: "Option #1"
+    },
+    {
+      id: '2',
+      label: "Option #2"
+    },
+    {
+      id: '3',
+      label: "Option #3"
+    },
+  ]
+  rows = ['1', '2', '3'];
 
   form: FormGroup = new FormGroup({
     input: new FormControl(''),
@@ -43,5 +76,13 @@ export class App {
   handleInput(event: Event) {
     const value = (event as CustomEvent).detail.value;
     this.inputValue.set(value);
+  }
+
+  handleRowSelection($event: CustomEvent<{value: string[]}>) {
+    this.selectedRows.set($event.detail.value);
+  }
+
+  getFieldDisableState(id: string) {
+    return !this.selectedRows().includes(id);
   }
 }
